b30cde91c248bdbf131f79d22b546eabcf4f6cbd
Jack Nguyen <J4ck.tf@gmail.com>
2023-12-21 21:26:29 -0700

    Add a feedforward controller class

--- Controllers/Feedforward.java
+++ Controllers/Feedforward.java
@@ -1,4 +1,16 @@
 package org.firstinspires.ftc.teamcode.Controllers;
 
 public class Feedforward {
+    public double Kf, currentOut;
+
+    public Feedforward(double Kf) {
+        this.Kf = Kf;
+    }
+
+    public double update(double input) {
+        currentOut = input * Kf;
+        return currentOut;
+    }
+
+    public double getOutput() { return currentOut; }
 }
